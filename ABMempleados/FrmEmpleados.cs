9e9c692a3d520f5d7fb7bb269ef5c23518b8a63a
facundo000 <facundo.guzman@mi.unc.edu.ar>
2024-06-06 11:08:09 -0300

    first commit

---
+++ ABMempleados/FrmEmpleados.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Windows.Forms;
+
+namespace TP_Parte_3
+{
+    public partial class FrmEmpleados : Form
+    {
+        public FrmEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
